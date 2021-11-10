832db24646fb4af9bd3b74fdff416e12f02c2926
siyoungoh <dev@syoh.net>
2021-11-11 08:53:29 +0900

    Refactoring - lec spring04-04 simple test without mokito

--- src/main/java/com/sparta/springcore/model/Product.java
+++ src/main/java/com/sparta/springcore/model/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.utils.URLValidator;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -43,7 +44,39 @@ public class Product extends Timestamped {
 	private Long userId;
 
 	// 관심 상품 생성 시 이용합니다.
+	// public Product(ProductRequestDto requestDto, Long userId) {
+	// 	// 관심상품을 등록한 회원 Id 저장
+	// 	this.userId = userId;
+	// 	this.title = requestDto.getTitle();
+	// 	this.image = requestDto.getImage();
+	// 	this.link = requestDto.getLink();
+	// 	this.lprice = requestDto.getLprice();
+	// 	this.myprice = 0;
+	// }
+
+	// For validation - spring04 TEST
 	public Product(ProductRequestDto requestDto, Long userId) {
+		// 입력값 Validation
+		if (userId == null || userId < 0) {
+			throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
+		}
+
+		if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
+			throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
+		}
+
+		if (!URLValidator.urlValidator(requestDto.getImage())) {
+			throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
+		}
+
+		if (!URLValidator.urlValidator(requestDto.getLink())) {
+			throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
+		}
+
+		if (requestDto.getLprice() <= 0) {
+			throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
+		}
+
 		// 관심상품을 등록한 회원 Id 저장
 		this.userId = userId;
 		this.title = requestDto.getTitle();
